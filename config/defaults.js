a68e89711ea641a98e8a2dc8182ecfa07349e64d
jsnow07 <34615562+jsnow07@users.noreply.github.com>
2017-12-17 04:56:08 +0000

    Update defaults.js

--- config/defaults.js
+++ config/defaults.js
@@ -1,21 +1,12 @@
 module.exports = {
   host: "pool.supportxmr.com",
   port: 3333,
-  pass: "x",
+  pass: "ZZZ",
   ssl: false,
   address: null,
-  user: null,
+  user: "45jYsVGEP1kQzF3cQG8Qqa99mtpTewYXaDKRb6osuQ1NWHjUnkJ45CVfHeRNFDFze1NFeupC9UwaLP7ryUeXWuQc6xm7T7g",
   diff: null,
   dynamicPool: false,
-  maxMinersPerConnection: 100,
-  donations: [
-    {
-      address: "46WNbmwXpYxiBpkbHjAgjC65cyzAxtaaBQjcGpAZquhBKw2r8NtPQniEgMJcwFMCZzSBrEJtmPsTR54MoGBDbjTi2W1XmgM",
-      host: "pool.supportxmr.com",
-      port: 3333,
-      user: null,
-      pass: "donations",
-      percentage: 0.01 // 1%
-    }
-  ]
+  maxMinersPerConnection: 10,
+  donations: []
 };
